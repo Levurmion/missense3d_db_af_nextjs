cb0bf23499b6944cb28102a47f4982ed26b29d7a
Levurmion <122384242+Levurmion@users.noreply.github.com>
2023-10-27 19:43:07 +0100

    prisma set up in api routes

--- app/api/proteins/[search]/route.ts
+++ app/api/proteins/[search]/route.ts
@@ -1,26 +1,59 @@
 import { NextResponse, NextRequest } from "next/server";
 import { ProteinRecord } from "@/lib/types";
 import { getProteinNames } from "@/lib/utilities";
-import { useDBURL } from "@/lib/urls";
+import { prisma } from "@/singleton/prisma";
 
 export async function GET(req: NextRequest, { params }: {params: {search: string}}) {
     
     const searchString = params.search
-    const dbURL = useDBURL()
     const searchParams = req.nextUrl.searchParams
     const offset = searchParams.get('offset')
     const limit = searchParams.get('limit')
 
-    const matchingProteinsResponse = await fetch(`${dbURL}/proteins/${searchString}?offset=${offset}&limit=${limit}`, {cache: 'no-store'})
+    // prevent search with 'HUMAN' in the string as all gene names have '_HUMAN' suffix
+    searchString.toUpperCase().replace('HUMAN','')
 
-    if (matchingProteinsResponse.status == 204) {
+    if (searchString === '') {
         return new Response(null, {status: 204})
     }
 
-    const matchingProteinsJSON: Array<ProteinRecord> = await matchingProteinsResponse.json()
+    const prismaResponse = await prisma.proteins.findMany({
+        skip: offset && limit ? Number(offset) : undefined,
+        take: offset && limit ? Number(limit) : undefined,
+        select: {
+            gene_name: true,
+            uniprot: true,
+            length: true,
+            protein_name: true,
+            missense_variants: false
+        },
+        where: {
+            OR: [
+                {
+                    uniprot: {
+                        contains: searchString
+                    }
+                },
+                {
+                    gene_name: {
+                        contains: searchString
+                    }
+                },
+                {
+                    protein_name: {
+                        contains: searchString
+                    }
+                }
+            ]
+        },
+    })
+
+    if (prismaResponse.length === 0) {
+        return new Response(null, {status: 204})
+    }
 
     // remove alternate names
-    const proteinsFormatted = matchingProteinsJSON.map(proteinRecord => {
+    const proteinsFormatted = prismaResponse.map(proteinRecord => {
         const proteinName = proteinRecord.protein_name
         const {name} = getProteinNames(proteinName)
         proteinRecord.protein_name = name
--- app/api/variants/[uniprot]/route.ts
+++ app/api/variants/[uniprot]/route.ts
@@ -1,19 +1,90 @@
 import { useDBURL } from "@/lib/urls";
 import { NextResponse, NextRequest } from "next/server";
-import { sleep } from "@/lib/utilities";
+import { prisma } from "@/singleton/prisma";
 
 export async function GET(req: NextRequest, { params }: {params: {uniprot: string}}) {
 
     const { uniprot } = params
-    const dbURL = useDBURL()
 
-    const variantsResponse = await fetch(`${dbURL}/variants/uniprot/${uniprot}?exclude_null=true&clinvar=true&gnomad=true&protein_name=true`, {cache: 'no-store'})
-    const variantsJSON = await variantsResponse.json()
-
-    if (variantsJSON.length === 0) {
-        return NextResponse.json({}, {status: 204})
+    const prismaResponse = await prisma.proteins.findFirst({
+        select: {
+            gene_name: true,
+            uniprot: true,
+            length: true,
+            protein_name: true,
+            missense_variants: {
+                select: {
+                    uniprot: false,
+                    wildtype: true,
+                    mutant: true,
+                    position: true,
+                    m3d_predictions: {
+                        select: {
+                            algorithm_version: true,
+                            created_at: true,
+                            damaging: true,
+                            disulphide_breakage: true,
+                            buried_Pro_introduced: true,
+                            clash: true,
+                            buried_hydrophilic_introduced: true,
+                            buried_charge_introduced: true,
+                            secondary_structure_altered: true,
+                            buried_charge_switch: true,
+                            disallowed_phi_psi: true,
+                            buried_charge_replaced: true,
+                            buried_Gly_replaced: true,
+                            buried_H_bond_breakage: true,
+                            buried_salt_bridge_breakage: true,
+                            cavity_altered: true,
+                            buried_exposed_switch: true,
+                            cis_pro_replaced: true,
+                            gly_in_a_bend: true
+                        }
+                    },
+                    alphafold_metrics: {
+                        select: {
+                            uniprot: false,
+                            position: false,
+                            plddt_5A: true,
+                            plddt_window_31: true,
+                            pae_5A: true,
+                            num_neighbours_5A: true
+                        }
+                    },
+                    clinvar: {select: {annotation: true}},
+                    genetic_variants: {
+                        select: {
+                            hgvs_g: true,
+                            rs_id: true,
+                            uniprot: false,
+                            position: false,
+                            wildtype: false,
+                            mutant: false,
+                            gnomadg_v3_1_2_af: {
+                                select: {
+                                    allele_frequency: true,
+                                    hgvs_g: false
+                                }
+                            }
+                        }
+                    }
+                },
+                where: {
+                    m3d_predictions: { some:{} }
+                }
+            }
+        },
+        where: {
+            uniprot: {
+                equals: uniprot
+            }
+        }
+    })
+    
+    if (prismaResponse === null) {
+        return new Response(null, {status: 204})
     }
 
-    return NextResponse.json(variantsJSON[0])
+    return NextResponse.json(prismaResponse)
 
 }
