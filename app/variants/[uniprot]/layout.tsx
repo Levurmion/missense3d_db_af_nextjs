b0eb9825a5b793f374e14be7758d0fb4bb9eb770
Levurmion <122384242+Levurmion@users.noreply.github.com>
2023-10-30 13:39:20 +0000

    centered variants loading screen

--- app/variants/[uniprot]/layout.tsx
+++ app/variants/[uniprot]/layout.tsx
@@ -19,7 +19,7 @@ export default async function VariantsLayout({ children, params }: { children: R
 
         return (
             <main className='flex flex-col w-full items-center gap-6'>
-                <div className="flex flex-col w-full lg:w-fit gap-6">
+                <div className="flex flex-col items-center w-full lg:w-fit gap-6">
                     {children}
                 </div>
             </main>
